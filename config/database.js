303f21ec9f43f231202850cf6fd0d7877960f18f
Shubhankar Dubey <dubeyshubhankar007@gmail.com>
2018-04-25 22:42:55 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   // configure the code below with your username, password and mlab database information
-  database: 'mongodb://<username>:<password>@ds12226.mlab.com:12226/meanauthapp',   //prod
+  database: 'mongodb://shubh:shubh@ds259089.mlab.com:59089/meanauthapp12',   //prod
   //database: 'mongodb://localhost:27017/meanauth',    //dev
   secret: 'yoursecret'
 }
